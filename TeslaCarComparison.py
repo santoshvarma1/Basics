f44ca8943ceb1080d348e018d7d9d110ca3588e4
Santosh Varma Saripella <svs0395@gmail.com>
2022-03-01 17:27:44 -0500

    removedPrintStatements

--- TeslaCarComparison.py
+++ TeslaCarComparison.py
@@ -11,19 +11,15 @@ for each in csvreader:
     results.append(each)
 
 
-print(results)
-print(header)
-
 totalStash = 0
 totalWeeks = 0
 for row in results:
-    print(row)
     priceOfWeek = (float(row[2])+float(row[3]))/2
     StockPurchasedPerWeek = 100/priceOfWeek
     totalStash = totalStash + StockPurchasedPerWeek
-    print(f'purched stock this week {StockPurchasedPerWeek}, totalling stash at {totalStash}')
     if row == results[-1]:
         print(f'current value of the stock is at {totalStash*priceOfWeek*1.28} cad')
         print(f'total weeks {totalWeeks}: and total investment at {totalWeeks*100}')
     totalWeeks += 1
+
 
